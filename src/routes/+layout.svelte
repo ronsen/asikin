6dc364eb0c2376181331704c2f451bd570775888
Ronald Nababan <ronsen@startrek.or.id>
2023-11-15 15:10:00 +0700

    added logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,12 @@
     <title>Asikin</title>
 </svelte:head>
 
-<div class="navbar bg-base-300">
+<nav class="navbar bg-base-300">
     <div class="flex-1">
-        <a href="/" class="btn btn-ghost text-xl">Asikin</a>
+        <a href="/" class="btn btn-ghost text-xl">
+            <img src="/logo.png" alt="[]" class="w-6 h-6">
+            Asikin
+        </a>
     </div>
     <div class="flex-none">
         <form action="/" method="get">
@@ -31,12 +34,20 @@
                     <option value="GB">United Kingdom</option>
                     <option value="US">United States</option>
                 </select>
-                <button class="btn btn-primary btn-sm join-item">Change</button>
+                <button class="btn btn-neutral btn-sm join-item">Change</button>
             </div>
         </form>
     </div>
-</div>
+</nav>
 
 <main class="w-full md:w-4/5 mx-auto p-6 md:p-10">
     <slot />
 </main>
+
+<footer class="flex justify-center mb-6 md:mb-10">
+    <a href="https://s.id/ronsen" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-slash" viewBox="0 0 16 16">
+            <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+        </svg>
+    </a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
     export let data: PageServerData;
 </script>
 
+<h2 class="text-lg font-bold border-b border-base-300 pb-4 mb-4">
+    <a href="/?gl={data.gl}">{data.gl}</a>
+</h2>
+
 {#each data.videos as video}
-    <div class="video flex flex-row items-start gap-4 mb-6">
+    <div class="video flex flex-row items-start gap-4 border-b border-base-300 pb-4 mb-4">
         <div class="flex-none">
             <a href={video.url}
                 ><img
