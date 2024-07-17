4eced29a1927220bafde36707c2aecfcce2cc11b
Ronsen <ronsen@startrek.or.id>
2024-07-17 15:26:49 +0700

    change: black theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("daisyui")
+	],
+	daisyui: {
+		themes: ['black']
+	}
 }
 
