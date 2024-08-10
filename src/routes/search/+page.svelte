8eefe2765181bc8448a5fa20bcfdcf86ed050da7
Ronsen <ronsen@startrek.or.id>
2024-08-10 10:12:38 +0700

    update

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -5,6 +5,10 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<title>{data.q}</title>
+</svelte:head>
+
 <h2 class="text-lg font-bold border-b border-base-300 pb-3 mb-6">
 	<a href="/search?q={data.q}">{data.q}</a>
 </h2>
